b62167a3bdf8cee18340ac0226f40407229c76b8
mohammad <mohammad.owda@fis.ps>
2022-07-27 15:35:11 +0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/frontEnd/'
+    : '/',
+  devServer: {
+    disableHostCheck: true,
+  },
+
+  transpileDependencies: ['vuetify'],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+}
